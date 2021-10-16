0d8eae4a8aebbff02dc073d4bd342a1338f45b02
Alex <boytunghc@gmail.com>
2021-10-16 23:38:27 +0700

    add app router

---
+++ app/Services/RouterService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use Core\Route;
+use Core\Services\RouterService as CoreRouterService;
+
+class RouterService extends CoreRouterService
+{
+    public function registerRoutes(Route $route)
+    {
+
+    }
+}
