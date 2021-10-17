6539f9848bb3ea203acea73b39e7cf4d0fe2624e
Alex <boytunghc@gmail.com>
2021-10-17 17:08:05 +0700

    add event handle

--- core/Http/Auth.php
+++ core/Http/Auth.php
@@ -6,19 +6,43 @@ class Auth
 {
     protected $user;
 
+    protected $events = [
+        'login' => [],
+        'logout' => []
+    ];
+
     public function login($user)
     {
-        $this->user = $user;
+        $this->dispatch('login', $this->user = $user);
     }
 
     public function logout()
     {
+        $this->dispatch('logout', $this->user);
+
         return $this->user = null;
     }
 
     public function isLogged()
     {
         return $this->user !== null;
+    }
+
+    public function onLogin(callable $callback)
+    {
+        $this->events['login'] = $callback;
+    }
+
+    public function onLogout(callable $callback)
+    {
+        $this->events['logout'] = $callback;
+    }
+
+    protected function dispatch(string $event, ...$args)
+    {
+        foreach ($this->events[$event] as $callback) {
+            $callback(...$args);
+        }
     }
 
     public function user()
