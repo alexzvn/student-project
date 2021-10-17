31424a36c38ee95b92c9418446a4eb039499f439
Alex <boytunghc@gmail.com>
2021-10-17 18:56:45 +0700

    update login page

--- view/auth/login.php
+++ view/auth/login.php
@@ -3,7 +3,7 @@
 <div class="container mx-auto min-h-screen grid place-items-center">
     <div class="card shadow-lg w-full max-w-xl">
         <form class="card-body bg-white" method="POST" action="/login">
-            <h1 class="text-center font-semibold text-2xl">Đăng nhập</h1>
+            <h1 class="text-center font-semibold text-2xl">Sign in to xShop</h1>
 
             <div class="form-control">
                 <label class="label"><span class="label-text">Email</span></label>
@@ -14,7 +14,11 @@
                 <input name="password" type="password" placeholder="Your password" class="input input-primary input-bordered" required>
             </div>
 
-            <button type="submit" class="btn mt-5">Đăng nhập</button> 
+            <span class="mt-4 text-gray-500 text-sm">
+                Need an account? <a href="/register" class="text-blue-400">Create new account</a>
+            </span>
+
+            <button type="submit" class="btn mt-5">Sign in</button> 
         </form>
     </div>
 </div>
