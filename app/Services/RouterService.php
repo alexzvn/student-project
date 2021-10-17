b5d356588397dcaeb712610230dc712592321778
Alex <boytunghc@gmail.com>
2021-10-17 18:16:30 +0700

    add routes for auth

--- app/Services/RouterService.php
+++ app/Services/RouterService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Controllers\Auth\LoginController;
+use App\Controllers\Auth\RegisterController;
 use App\Controllers\HomeController;
 use Core\Route;
 use Core\Services\RouterService as CoreRouterService;
@@ -11,5 +13,11 @@ class RouterService extends CoreRouterService
     public function registerRoutes(Route $route)
     {
         $route->get('/', [HomeController::class, 'index']);
+
+        $route->get('/login', [LoginController::class, 'index']);
+        $route->post('/login', [LoginController::class, 'login']);
+        $route->get('/register', [RegisterController::class, 'index']);
+        $route->post('/register', [RegisterController::class, 'register']);
+        $route->get('/logout', [LoginController::class, 'logout']);
     }
 }
