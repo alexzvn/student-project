837f83ce208831ac518ff289c69fe7c0007d4e15
Alex <boytunghc@gmail.com>
2021-10-18 19:01:11 +0700

    add routes for manager

--- app/Services/RouterService.php
+++ app/Services/RouterService.php
@@ -3,8 +3,14 @@
 namespace App\Services;
 
 use App\Controllers\Auth\LoginController;
+use App\Controllers\Auth\LogoutController;
 use App\Controllers\Auth\RegisterController;
 use App\Controllers\HomeController;
+use App\Controllers\Manager\OrderController;
+use App\Controllers\Manager\ProductController;
+use App\Controllers\UserController;
+use App\Models\User;
+use Core\Http\Auth;
 use Core\Route;
 use Core\Services\RouterService as CoreRouterService;
 
@@ -18,6 +24,30 @@ class RouterService extends CoreRouterService
         $route->post('/login', [LoginController::class, 'login']);
         $route->get('/register', [RegisterController::class, 'index']);
         $route->post('/register', [RegisterController::class, 'register']);
-        $route->get('/logout', [LoginController::class, 'logout']);
+        $route->get('/logout', [LogoutController::class, 'logout']);
+
+        $route->get('/cart', [HomeController::class, 'cart']);
+        $route->get('/checkout', [HomeController::class, 'checkout']);
+
+        $this->registerPrivateRoutes($route, auth());
+    }
+
+    protected function registerPrivateRoutes(Route $route, Auth $auth)
+    {
+        if ($auth->isLogged() === false) return;
+        $route->get('/me', [UserController::class, 'index']);
+        $route->get('/me/orders', [UserController::class, 'order']);
+
+        if (! $auth->user()->is_admin) return;
+        $route->get('/manager/products', [ProductController::class, 'index']);
+        $route->get('/manager/products/create', [ProductController::class, 'create']);
+        $route->post('/manager/products/store', [ProductController::class, 'store']);
+        $route->get('/manager/products/{id}', [ProductController::class, 'show']);
+        $route->post('/manager/products/{id}/update', [ProductController::class, 'update']);
+        $route->post('/manager/products/{id}/delete', [ProductController::class, 'delete']);
+
+        $route->get('/manager/orders', [OrderController::class, 'index']);
+        $route->get('/manager/orders/{id}', [OrderController::class, 'show']);
+        $route->post('/manager/orders/{id}/delete', [OrderController::class, 'delete']);
     }
 }
