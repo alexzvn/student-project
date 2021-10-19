c76d3937ea323b5bce0e646a872b22c7b85dd0a0
Alex <boytunghc@gmail.com>
2021-10-19 18:24:58 +0700

    add fulltext maker

--- core/Model/BaseModel.php
+++ core/Model/BaseModel.php
@@ -34,4 +34,27 @@ abstract class BaseModel extends SimpleOrm
 
         return $this;
     }
+
+    public static function fuzzyText(string $term)
+    {
+        // removing symbols used by MySQL
+        $reservedSymbols = ['-', '+', '<', '>', '@', '(', ')', '~', "'", '"'];
+        $term = str_replace($reservedSymbols, '', $term);
+ 
+        $words = explode(' ', $term);
+ 
+        foreach ($words as $key => $word) {
+            /*
+             * applying + operator (required word) only big words
+             * because smaller ones are not indexed by mysql
+             */
+            if (strlen($word) >= 2) {
+                $words[$key] = "'+$word*'";
+            }
+        }
+ 
+        $searchTerm = implode(' ', $words);
+ 
+        return $searchTerm;
+    }
 }
