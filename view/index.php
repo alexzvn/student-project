42c5f3f71797e2740888683c84a29fe9297b5a24
Alex <boytunghc@gmail.com>
2021-10-19 21:05:44 +0700

    add alert

--- view/index.php
+++ view/index.php
@@ -9,7 +9,9 @@
             <h2 class="font-semibold text-lg uppercase text-center">All products</h2>
         </div>
         <div class="card-body bg-white">
-            <div class="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+            <?php include_view('layouts.alert') ?>
+
+            <div class="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 mt-3">
                 <?php
                     foreach ($products as $product) include_view('components.product', compact('product'));
                 ?>
