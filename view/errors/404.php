ec59603a5e4d5f9d8126de3c0d5dc6750aee2cf6
Alex <boytunghc@gmail.com>
2021-10-19 15:31:19 +0700

    styling 404 page

--- view/errors/404.php
+++ view/errors/404.php
@@ -2,8 +2,13 @@
 
 <div class="container mx-auto min-h-screen grid place-items-center">
     <div class="card shadow-lg w-full max-w-xl">
-        <div class="card-body bg-white">
-            <h1 class=" font-thin text-6xl">404</h1>
+        <div class="card-body bg-white text-center">
+            <h1 class="font-thin text-9xl">4<span class="font-normal text-yellow-400">0</span>4</h1>
+            <h2 class="mt-3 font-semibold text-xl">Not found resource you're looking for!</h2>
+
+            <div class="mt-10">
+                <a href="/" class="btn btn-lg btn-primary btn-outline border-dashed">◀︎ Back To Home</a>
+            </div>
         </div>
     </div>
 </div>
