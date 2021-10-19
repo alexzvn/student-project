935dfc66a5f03aa3a1dc967d55c40f03d57275d8
Alex <boytunghc@gmail.com>
2021-10-19 17:43:38 +0700

    add data to home layouts

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controllers;
 
+use App\Models\Product;
 use Core\Http\Request;
 use Core\Http\Response;
 
 class HomeController
 {
-    public function index(Request $request)
+    public function index()
     {
-        return view('index');
+        return view('index', [
+            'products' => Product::all()
+        ]);
     }
 }
