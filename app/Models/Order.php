5ec09a6d4a984916ef6110c83a59e33f1fb92ad1
Alex <boytunghc@gmail.com>
2021-10-19 22:21:59 +0700

    add relationship

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Core\Model\BaseModel;
+
+class Order extends BaseModel
+{
+    protected static $table = 'orders';
+
+    protected $fillable = [
+        'address'
+    ];
+
+    public function products()
+    {
+        return OrderItems::sql("SELECT * FROM products
+            RIGHT JOIN order_items ON order_items.product_id = products.id
+            WHERE order_id = $this->id");
+    }
+}
